33b2d8706dbdbd2d3791dc775ace7698fec07cd5
Lasisi Ibrahim <ilasisi90@gmail.com>
2018-06-29 17:38:43 -0700

    Initial Commit

---
+++ app/src/main/java/mydiary/bloomydev/ilasisi/mydiary/DetailsDiary.java
@@ -0,0 +1,73 @@
+package mydiary.bloomydev.ilasisi.mydiary;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.widget.TextView;
+
+public class DetailsDiary extends Activity {
+
+    private static final String TAG = "notepad";
+
+    //our views for display note title and content
+    private TextView noteTitle;
+    private TextView createdAt;
+    private TextView noteContent;
+
+    //dbhelper
+    private DatabaseHelper dbhelper;
+    private SQLiteDatabase db;
+
+    //default values for title and content variables
+    private String title = "defaultTitle";
+    private int id = 0;
+    private String content = "defaultContent";
+    private String date = "date";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_details_diary);
+
+        //initialization of DBHelper
+        dbhelper = new DatabaseHelper(getApplicationContext());
+
+
+        noteTitle = (TextView) findViewById(R.id.noteTitle);
+        noteContent = (TextView) findViewById(R.id.noteContent);
+        createdAt = (TextView) findViewById(R.id.createdAt);
+
+        // getting intent
+        Intent mIntent = getIntent();
+
+        id = mIntent.getIntExtra("id", 0);
+
+
+        //getting the readable database
+        db = dbhelper.getReadableDatabase();
+
+        //getting the note from database
+        Cursor c = dbhelper.getNote(db, id);
+        //closing the database connection
+        db.close();
+
+        //getting the content from cursor
+        //getString(1) because first column is noteTitle and second is noteContent and the third column is date
+        title = c.getString(0).toString();
+        content = c.getString(1).toString();
+        date = c.getString(2).toString();
+
+        //setting notes to our views
+        noteTitle.setText(title);
+        noteContent.setText(content);
+        createdAt.setText(date);
+    }
+
+
+}
--- app/src/main/java/mydiary/bloomydev/ilasisi/mydiary/MainActivity.java
+++ app/src/main/java/mydiary/bloomydev/ilasisi/mydiary/MainActivity.java
@@ -1,52 +1,202 @@
 package mydiary.bloomydev.ilasisi.mydiary;
 
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements
+        OnItemClickListener {
 
+    // Tag for debugging
+    private static final String TAG = "notepad";
+
+    // our views from layout
+    private ListView noteList;
+    private Button addNoteBtn;
+
+    // adapter use to populate the listview
+    private ArrayAdapter<String> adapter;
+    // cursor will contain notes from database
+    private Cursor notes;
+    // database helper
+    private DatabaseHelper dbhelper;
+
+    // items contain notes titles
+    private ArrayList<String> titles;
+    private ArrayList<Item> items;
+
+    // variable will contain the position of clicked item in listview
+    private int position = 0;
+
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        noteList = (ListView) findViewById(R.id.noteList);
+        addNoteBtn = (Button) findViewById(R.id.addNote);
+
+        // initialization of database helper
+        dbhelper = new DatabaseHelper(getApplicationContext());
+
+        // setting note's titles to item in listview
+        setNotes();
+
+        // setting that longclick on listview will open the context menu
+        this.registerForContextMenu(noteList);
+
+        // onClicklistener for our "new note" button
+        // click on button will open CreateNote Activity
+        addNoteBtn.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),
+                        AddNewNote.class));
+
             }
         });
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    //
+    public void setNotes() {
+        // init the items arrayList
+        titles = new ArrayList<String>();
+        items = new ArrayList<Item>();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        // getting readable database
+        SQLiteDatabase db = dbhelper.getReadableDatabase();
+        // getting notes from db
+        // see dbhelper for more details
+        notes = dbhelper.getNotes2(db);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // this should fix the problem
+        // now the activity will be managing the cursor lifecycle
+        startManagingCursor(notes);
+
+        // closing database connection !important
+        // always close connection with database
+        // we closing database connection here because we don't use db anymore
+//        db.close();
+
+        // populating ArrayList items with notes titles
+        if (notes.moveToFirst()) {
+            do {
+                items.add(new Item(notes.getShort(0), notes.getString(1)));
+            } while (notes.moveToNext());
         }
 
-        return super.onOptionsItemSelected(item);
+
+        for (Item i : items) {
+            titles.add(i.getTitle());
+        }
+
+        // creating new adapter
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, titles);
+        noteList.setAdapter(adapter);
+        // setting listener to the listView
+        noteList.setOnItemClickListener(this);
+
     }
+
+    // always when we start this activity we want to refresh the list of notes
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setNotes();
+    }
+
+    // this method is called when user long clicked on listview
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        // using menuInfo to determine which item of listview was clicked
+        AdapterContextMenuInfo info = (AdapterContextMenuInfo) menuInfo;
+        position = info.position;
+        // setting the title of context menu header
+        menu.setHeaderTitle(getResources().getString(R.string.CtxMenuHeader));
+        // inflating the menu from xml file
+        // for details see context_menu.xml file in /res/menu folder
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.context_menu, menu);
+
+    }
+
+    // method is called when user clicks on contextmenu item
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        // getting the textView from ListView which item was clicked
+        TextView tv = (TextView) noteList.getChildAt(position);
+        // getting the title of this textView
+        String title = tv.getText().toString();
+
+        // performing one of actions, depending on user choice
+        switch (item.getItemId()) {
+
+            case R.id.showNote:
+//                Intent mIntent = new Intent(this, DetailsDiary.class);
+//                mIntent.putExtra("id", items.get(position).getId());
+//                startActivity(mIntent);
+                Intent itnt = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(itnt);
+                break;
+
+            case R.id.editNote:
+                Intent i = new Intent(this, AddNewNote.class);
+                i.putExtra("id", items.get(position).getId());
+                Log.d(TAG, title);
+                // this is important
+                // we send boolean to CreateNote activity
+                // thanks to this boolean activity knows that user want to edit
+                // notes
+                i.putExtra("isEdit", true);
+                startActivity(i);
+                break;
+
+            case R.id.removeNote:
+                // removing this notes
+                dbhelper.removeNote(items.get(position).getId());
+                // refreshing the listView
+                setNotes();
+                break;
+
+        }
+
+        return false;
+
+    }
+
+    // when user click on note's title we're opening this note in OneNote
+    // activity
+    @Override
+    public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+        TextView tv = (TextView) arg1;
+        String title = tv.getText().toString();
+        Intent mIntent = new Intent(this, DetailsDiary.class);
+        mIntent.putExtra("title", title);
+        mIntent.putExtra("id", items.get(arg2).getId());
+        startActivity(mIntent);
+    }
+
 }
